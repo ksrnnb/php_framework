4fe1b7c3bfc12868e6c083f361a1ae6c486a43fb
ksrnnb <enrico.fermi.phys@gmail.com>
2021-05-06 06:33:25 +0900

    refactor: モデルのインスタンス化処理をDBクラスに移動 #6

--- framework/helpers/Collection.php
+++ framework/helpers/Collection.php
@@ -2,41 +2,18 @@
 
 class Collection
 {
-    private $models = [];
-    private $name;
+    private $data = [];
 
-    public function __construct($properties, $model_name)
+    public function __construct($data)
     {
-        $this->name = $model_name;
-        $this->resolveModels($properties);
+        $this->data = $data;
     }
 
-    private function resolveModels($properties)
-    {
-        $resolved = [];
-        foreach ($properties as $property) {
-            $resolved[] = $this->resolveModel($property);
-        }
-
-        $this->models = $resolved;
-    }
-
-    private function resolveModel($property)
-    {
-        $class = $this->name;
-
-        $model = new $class();
-        foreach ($property as $key => $value) {
-            $model->$key = $value;
-        }
-
-        return $model;
-    }
 
     // TODO: イテレータ処理の実装
 
     public function first()
     {
-        return $this->models[0];
+        return $this->data[0];
     }
 }
